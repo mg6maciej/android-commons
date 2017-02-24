7ff30b21a5d558b17238f700b7461b40f3289eb9
Karol Kowalski <ordon1313@gmail.com>
2017-02-24 18:11:43 +0100

    Name velocity assertions

--- espresso/src/androidTest/java/com/elpassion/android/commons/espresso/SwipeTest.kt
+++ espresso/src/androidTest/java/com/elpassion/android/commons/espresso/SwipeTest.kt
@@ -21,25 +21,25 @@ class SwipeTest {
     @Test
     fun shouldBehaveWhenSwipedLeft() {
         onId(R.id.first).swipeLeft()
-        onId(R.id.first).hasTag(withVelocity({ it.x < -1.0f && abs(it.y) < 1.0f }, "x negative"))
+        onId(R.id.first).hasTag(withVelocity(velocityLeft, "x negative"))
     }
 
     @Test
     fun shouldBehaveWhenSwipedRight() {
         onId(R.id.first).swipeRight()
-        onId(R.id.first).hasTag(withVelocity({ it.x > 1.0f && abs(it.y) < 1.0f }, "x positive"))
+        onId(R.id.first).hasTag(withVelocity(velocityRight, "x positive"))
     }
 
     @Test
     fun shouldBehaveWhenSwipedUp() {
         onId(R.id.first).swipeUp()
-        onId(R.id.first).hasTag(withVelocity({ abs(it.x) < 1.0f && it.y < -1.0f }, "y negative"))
+        onId(R.id.first).hasTag(withVelocity(velocityUp, "y negative"))
     }
 
     @Test
     fun shouldBehaveWhenSwipedDown() {
         onId(R.id.first).swipeDown()
-        onId(R.id.first).hasTag(withVelocity({ abs(it.x) < 1.0f && it.y > 1.0f }, "y positive"))
+        onId(R.id.first).hasTag(withVelocity(velocityDown, "y positive"))
     }
 
     class FakeActivity : Activity() {
@@ -60,6 +60,11 @@ class SwipeTest {
     }
 
     data class Velocity(val x: Float, val y: Float)
+
+    private val velocityLeft: (Velocity) -> Boolean = { it.x < -1.0f && abs(it.y) < 1.0f }
+    private val velocityRight: (Velocity) -> Boolean = { it.x > 1.0f && abs(it.y) < 1.0f }
+    private val velocityUp: (Velocity) -> Boolean = { abs(it.x) < 1.0f && it.y < -1.0f }
+    private val velocityDown: (Velocity) -> Boolean = { abs(it.x) < 1.0f && it.y > 1.0f }
 
     private fun withVelocity(predicate: (Velocity) -> Boolean, predicateDescription: String)
             = createObjectMatcher<Any, Velocity>(predicate, { it.appendText("is Velocity with $predicateDescription") })
